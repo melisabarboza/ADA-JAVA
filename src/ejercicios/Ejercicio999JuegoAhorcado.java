727105a522be8abe79d4847fd5b5e8c983c88f03
Melisa <melisa.barboza@hotmail.com>
2020-09-17 22:00:05 -0300

    Ejercicios completos

--- src/ejercicios/Ejercicio999JuegoAhorcado.java
+++ src/ejercicios/Ejercicio999JuegoAhorcado.java
@@ -4,59 +4,105 @@ import java.util.Scanner;
 
 public class Ejercicio999JuegoAhorcado {
 
+	private static final int MIN_PALABRA = 4;
+	private static final int MAX_PALABRA = 10;
+	private static final int MAX_ERRORES = 10;
+
 	public static void main(String[] args) {
 		// generar palabra
 		String palabra = generarPalabra();
 
 		// repetir ingreso letras hasta adivinar o cantidad jugadas < 10
-		int intentos = adivinar(palabra);
+		int intentosFallidos = adivinar(palabra);
 
 		// mostrar mensaje de puntos
-		mostrarPuntos(intentos);
+		mostrarPuntos(intentosFallidos);
 	}
 
-	private static void mostrarPuntos(int intentos) {
-		// Calculo de puntaje en base a intentos (4 intentos -> mayor puntaje)
+	private static void mostrarPuntos(int intentosFallidos) {
+		// Calculo de puntaje en base a intentos (1 intentos -> mayor puntaje)
 		// 10 intentos -> sin puntaje
-		System.out.println("Puntaje obtenido: " + intentos);
-
+		int puntaje = MAX_PALABRA - intentosFallidos;
+		System.out.println("Puntaje obtenido: " + puntaje);
 	}
 
 	/**
-	 * Este metodo recibe la palabra a adivinar y devuelve el numero de intentos en
-	 * que fue adivinada la palabra
+	 * Este metodo recibe la palabra a adivinar y devuelve el numero de intentos
+	 * fallidos en que fue adivinada la palabra
 	 * 
 	 * @param palabra
 	 * @return
 	 */
 	private static int adivinar(String palabra) {
 		// pedir una letra
+		int aciertos = 0;
+		int errores = 0;
+		boolean[] resultado = new boolean[palabra.length()];
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese una letra");
 		String letra = sc.next();
 
 		// mientras cantidad letras acertadas < total letras
-		// o cantidad errores = 10
+		// y cantidad errores = 10
+		while (aciertos < palabra.length() && errores != MAX_ERRORES) {
 
-		// buscar esa letra en la palabra
-		int posicion = palabra.indexOf(letra); // si la letra no existe devuelve -1
+			// buscar esa letra en la palabra
 
-		// si existe mostrar letra, contar acierto
-		palabra.charAt(posicion); // devuelve la letra de la palabra en la posicion indicada
+			int posicion = palabra.indexOf(letra); // si la letra no existe devuelve -1
 
-		// sino mostrar _, contar error
+			if (posicion != -1) {
+				resultado[posicion] = posicion != -1;
+				aciertos++;
+			} else {
+				errores++;
+			}
 
-		// pedir letra
+			for (int i = 0; i < palabra.length(); i++) {
+				// si existe mostrar letra, contar acierto
+				if (resultado[i]) {
+					System.out.print(" " + palabra.charAt(i) + " ");
+				} else {
+					// sino mostrar _, contar error
+					System.out.print(" _ ");
+				}
+			}
+			// devuelve la letra de la palabra en la posicion indicada
 
-		sc.close();
-		return 0;
+			// mostrar _ O _ _ (Errores: 0)
+			System.out.println("Errores: " + errores);
+
+			// pedir letra
+			System.out.println("Ingrese una letra");
+			letra = sc.next();
+
+		}
+
+		if (errores == MAX_ERRORES) {
+			System.out.println("Siga participando!!!");
+		} else {
+			System.out.println("Ganaste!!!!");
+		}
+		return errores;
 	}
 
 	private static String generarPalabra() {
+		Scanner sc = new Scanner(System.in);
 		// solicitar ingreso palabra secreta
+		System.out.println("Ingrese palabra secreta");
+		String palabra = sc.next();
+
 		// minimo 4 letras
 		// maximo 10 letras
-		return null;
+		while (palabra.length() < MIN_PALABRA || palabra.length() > MAX_PALABRA) {
+			System.out.println("La palabra tiene que tener como mínimo " + MIN_PALABRA);
+			System.out.println("La palabra tiene que tener como máximo " + MAX_PALABRA);
+			System.out.println();
+			System.out.println("Ingrese palabra secreta");
+			palabra = sc.next();
+		}
+
+		return palabra;
 	}
 
 }
